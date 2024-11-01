77d66a010bbe05207b35b3330fd2b882c601dedc
bangbang <1789228622@qq.com>
2024-11-01 20:16:08 +0800

    gitChange

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 /*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-09-23 22:56:59
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-09-25 11:19:15
+ * @Author: bangbang 1789228622@qq.com
+ * @Date: 2024-09-24 13:56:59
+ * @LastEditors: bangbang 1789228622@qq.com
+ * @LastEditTime: 2024-11-01 20:15:38
  * @FilePath: /success2025/src/main.cpp
  * @Description:
  *
- * Copyright (c) 1999 by ${git_name_email}, All Rights Reserved.
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
  */
+
 #include "iostream"
 #include "CameraApi.h"
 
