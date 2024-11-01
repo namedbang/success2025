e73ac46a4871d05431120c064485f78ad494ff4b
bangbang <1789228622@qq.com>
2024-11-01 21:01:57 +0800

    hand

--- src/hardware/api/camera.cpp
+++ src/hardware/api/camera.cpp
@@ -1,3 +1,14 @@
+/*
+ * @Author: bangbang 1789228622@qq.com
+ * @Date: 2024-09-25 11:36:52
+ * @LastEditors: bangbang 1789228622@qq.com
+ * @LastEditTime: 2024-11-01 21:01:56
+ * @FilePath: /success2025/src/hardware/api/camera.cpp
+ * @Description:
+ *
+ * Copyright (c) 2024 by CDTU-Success, All Rights Reserved.
+ */
+
 #include "camera.hpp"
 
 bool BsaeCamera::CameraInit()
